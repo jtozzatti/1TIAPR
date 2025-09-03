f3b05ccb021d0e873dca0df76385eb7049bda26d
jtozzatti <zzatti@63gmail.com>
2025-09-02 22:41:17 -0300

    exercicio feito

---
+++ Aula 1/calculo_de_raio.py
@@ -0,0 +1,4 @@
+pi=3.14159
+raio=5
+area=pi*raio**2
+print("A area de circuferencia de raio {raio} é {area:.2f}")
---
+++ Aula 1/entrada_dados.py
@@ -0,0 +1,4 @@
+nome=input("qual o seu nome? ")
+
+print("Ola, " + nome + "! Seja bem vindo(a)!")
+print(f"OlA, {nome}! Seja bem vindo(a)!")
